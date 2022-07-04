cc9f3500056af89526991c7c12ae8be319d91c85
Shubham Kashyap <shubhamkashyapdev@gmail.com>
2022-07-04 20:23:50 +0530

    login page added

--- components/Common/Forms/Input/TextInput.jsx
+++ components/Common/Forms/Input/TextInput.jsx
@@ -1,3 +1,4 @@
+import { EyeIcon, EyeOffIcon } from "@heroicons/react/outline"
 import React from "react"
 import { FaCameraRetro } from "react-icons/fa"
 
@@ -10,8 +11,11 @@ const TextInput = ({
   placeholder,
   errorMsg,
   required,
-  type,
+  type = "text",
   inputClassName,
+  showPassword,
+  setShowPassword,
+  handleShowPassword,
 }) => {
   return (
     <div className="flex-1 my-[2px]">
@@ -27,7 +31,7 @@ const TextInput = ({
       <div className="relative">
         <input
           className={` w-full text-[16px] text-indigo-900 outline-none border-[2px] border-indigo-500 mb-1 py-2 px-2 rounded-md shadow-md ${inputClassName}`}
-          type={type ? type : "text"}
+          type={showPassword ? "text" : type}
           name={name}
           onChange={handleChange}
           value={value ? value : null}
@@ -39,6 +43,23 @@ const TextInput = ({
           <div className="absolute top-[30%] right-4">
             <FaCameraRetro className="text-indigo-700" size={20} />
           </div>
+        )}
+        {setShowPassword && showPassword ? (
+          <div
+            onClick={handleShowPassword}
+            className="absolute top-[30%] right-4 cursor-pointer"
+          >
+            <EyeIcon height={20} className="text-indigo-700" size={20} />
+          </div>
+        ) : setShowPassword && !showPassword ? (
+          <div
+            onClick={handleShowPassword}
+            className="absolute top-[30%] right-4 cursor-pointer"
+          >
+            <EyeOffIcon height={20} className="text-indigo-700" size={20} />
+          </div>
+        ) : (
+          <></>
         )}
       </div>
       <p className="text-red-700 text-[12px] ml-2">{errorMsg}</p>
---
+++ pages/login/index.jsx
@@ -0,0 +1,90 @@
+import { Section } from "@components/Common"
+import React, { useState } from "react"
+import Link from "next/link"
+import { TextInput } from "@components/Common"
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    password: "",
+    email: "",
+  })
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleChange = (e) => {
+    console.log(e.target.name, e.target.value)
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
+  const handleShowPassword = () => {
+    console.log(showPassword)
+    setShowPassword(!showPassword)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log({ formData })
+  }
+
+  const { email, password } = formData
+
+  return (
+    <div className=" bg-[url(/images/auth.png)] md:bg-[url()] bg-indigo-400  md:bg-white py-4 flex justify-center items-center w-full flex-col md:flex-row">
+      <div className="w-[50%]  my-[60px]">
+        <div className="w-[100%] md:w-[80%] max-w-[500px] drop-shadow-2xl mx-auto bg-white p-6">
+          <h3 className="text-2xl font-bold tracking-wide mb-4">Login</h3>
+          <p className="mb-2">If you don't have an account</p>
+          <p className="text-xl mb-8">
+            You can{" "}
+            <Link href="/register">
+              <a className="text-indigo-700 font-semibold">Register here</a>
+            </Link>
+          </p>
+          <form onSubmit={handleSubmit} className="">
+            <TextInput
+              name="email"
+              label="Your Email"
+              type="email"
+              value={email}
+              placeholder="Your Email"
+              required
+              handleChange={handleChange}
+            />
+            <TextInput
+              name="password"
+              label="Your Password"
+              type="password"
+              value={password}
+              placeholder="Your Password"
+              required
+              showPassword={showPassword}
+              setShowPassword={setShowPassword}
+              handleChange={handleChange}
+              handleShowPassword={handleShowPassword}
+            />
+            <button
+              type="submit"
+              className="py-2 w-full bg-indigo-700 text-white font-semibold tracking-wide text-center rounded-lg shadow-lg mt-8 hover:drop-shadow-md"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="w-[50%] hidden md:block px-8 bg-indigo-700 rounded-md drop-shadow-sm p-6">
+        <h4 className="mb-8 text-2xl font-bold text-white">
+          Login to continue
+        </h4>
+        <div className="">
+          <img className="w-full" src="/images/auth.png" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
