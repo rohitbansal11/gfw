a55c66dddee9d39a6c615b6779457025be12d07e
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2023-10-24 15:02:22 +0530

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://gudguru.herokuapp.com/api/v1",
+  baseURL: "https://gudguruapi.onrender.com/api/v1",
   //baseURL: "https://gud-guru-api-rohitbansal11.vercel.app/api/v1",
   // baseURL: "http://localhost:5000/api/v1",
 });
